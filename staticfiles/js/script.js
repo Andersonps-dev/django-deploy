f238e5f71d0d240ed5c2ca4fcb7626dd544d2eb4
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-04-01 15:04:00 -0300

    exclusão filesstatics

--- staticfiles/js/script.js
+++
@@ -1,9 +0,0 @@
-function removerProduto(element) {
-    element.parentElement.remove();
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("produtoForm").addEventListener("submit", function(event) {
-        alert("Produto adicionado com sucesso!");
-    });
-});
