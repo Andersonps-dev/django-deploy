6179669acb2732424696df3be7db71e02c96b21f
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-04-02 15:11:26 -0300

    `Added functionality to delete products from the list, updated templates and views accordingly.`

--- deploy_app/urls.py
+++ deploy_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(name='index')),   
+    path('deletar_produto/<int:pk>/', ProdutoDeleteView.as_view(name='deletar_produto')),   
 ]
--- deploy_app/views.py
+++ deploy_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Produto
 from .forms import ProdutoForm
-from django.views.generic import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DeleteView
+from django.urls import reverse_lazy
+from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produtos'] = Produto.objects.all()
+        context['form'] = ProdutoForm()
+        return context
+
+class ProdutoDeleteView(DeleteView):
+    model = Produto
+    success_url = reverse_lazy('index')
+    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return HttpResponse(status=204)
+    
+       
